b8aac2f2b574b5f30ef517eaf65a6e382e51db89
wasdodi <103326512+wasdodi@users.noreply.github.com>
2022-04-09 22:58:32 +0700

    First Setting Powershell Terminal
    Setting my own terminal in windows

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+$ENV:STARSHIP_CONFIG = "C:\Users\wasdo\.starship\starship.toml"
+$ENV:STARSHIP_DISTRO = 
+"█▀▀ █▀█ ▀█▀ █░░ ▄▀█ █▄▄
+█▄█ █▄█ ░█░ █▄▄ █▀█ █▄█ "
+Invoke-Expression (&starship init powershell)
